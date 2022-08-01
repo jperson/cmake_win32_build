076708ecc378e714cdafe953c50edd3fd73b00a9
Jason Person <jrperson@gmail.com>
2022-07-31 22:41:59 -0700

    use spdlog

--- libs/dbgcam/src/dbgcam.cpp
+++ libs/dbgcam/src/dbgcam.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include <boost/log/trivial.hpp>
+//#include <boost/log/trivial.hpp>
+#include <spdlog/spdlog.h>
 
 #include <pvcam/master.h>
 #include <pvcam/pvcam.h>
 
 rs_bool PV_DECL pl_pvcam_init(void) {
-    BOOST_LOG_TRIVIAL(info) << "pl_pvcam_init";
+    //BOOST_LOG_TRIVIAL(info) << "pl_pvcam_init";
+    spdlog::info("pl_pvcam_init");
     return PV_OK;
 }
--- src/app/src/main.cpp
+++ src/app/src/main.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
-#include <boost/log/trivial.hpp>
+//#include <boost/log/trivial.hpp>
+#include <spdlog/spdlog.h>
 
 #include <pvcam/master.h>
 #include <pvcam/pvcam.h>
 
 int main(int argc, char* argv[]) {
-    BOOST_LOG_TRIVIAL(info) << "Running";
+    //BOOST_LOG_TRIVIAL(info) << "Running";
+    spdlog::info("Running");
     pl_pvcam_init();
 }
